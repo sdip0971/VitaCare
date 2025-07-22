8a8a0539703e8ae8f8a38c70742fdbf59b42c503
sdip0971 <shdipanshu4@gmail.com>
2025-07-22 23:42:23 +0530

    added onboarded flag for patient

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { getApps, initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtlPzcLLCfhwMBcN12zstjsaR3-Tv5Fzs",
-  authDomain: "vitacare-de073.firebaseapp.com",
-  projectId: "vitacare-de073",
-  storageBucket: "vitacare-de073.firebasestorage.app",
-  messagingSenderId: "852564741249",
-  appId: "1:852564741249:web:6f28499952156d88415180",
-  measurementId: "G-HEL3WN0H1S",
+  apiKey: "AIzaSyBr-cRzyAgdRnzBfcJPVq3_ZjuI2t4p-mE",
+  authDomain: "vitacare-a33f2.firebaseapp.com",
+  projectId: "vitacare-a33f2",
+  storageBucket: "vitacare-a33f2.firebasestorage.app",
+  messagingSenderId: "510808638072",
+  appId: "1:510808638072:web:6d1d473218ea533ff1a38d",
+  measurementId: "G-WKZYZ39QMC",
 };
-// Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+console.log(firebaseConfig.apiKey)
+const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+if (typeof window !== "undefined") {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+      "6LcmzosrAAAAACfr2qdGEjmFg9zu9dzrpxKw01ze"
+    ),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
 export const auth = getAuth(app);
